d4048cab55751bcd5f63d7f053177da8a773e533
Mavrk24 <84179997+Mavrk24@users.noreply.github.com>
2021-09-07 15:06:33 +0700

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      default: "Username"
+    },
+    email: {
+      type: String,
+      required: true,
+      default: "E-mail"
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    },
+    result: {
+      type: Array,
+      require: true
+    },
+    demographic: {
+      type: Object,
+      require: true,
+      default: {}
+    },
+    ndi: {
+      type: Array,
+      require: true,
+      default: [] 
+    },
+    workplace: {
+      type: Array,
+      require: true,
+      default: 0
+    },
+    qtree: {
+      type: Array,
+      require: true,
+      default: []
+    },
+    recommendation: {
+      type: Array,
+      require: true,
+      dafault: []
+    } // end here
+  },
+
+  {
+    // กำหนด collection ของ MongoDB หรือจะไม่กำหนดก็ได้
+    collection: "user_data"
+  }
+  );
+
+module.exports = User = mongoose.model("users", UserSchema);
