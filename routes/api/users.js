1a2d54078818a8db13ee5354f865ce5f526af8e4
KitRat <65643227+KitRat@users.noreply.github.com>
2021-09-08 19:42:16 +0700

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,13 +10,13 @@ const validateLoginInput = require("../../validation/login");
 const User = require("../../models/User");
 const authorization = require('../../config/auth')
 
-const db = require("../config/keys").mongoURI;
+const db = 333;
 
 router.post('/test', (req, res) => {
  res.status(201).json({
   1: req.body.username,
   2: req.body.password
-  //3: JSON.stringify(db)
+  3: JSON.stringify(db)
  })
   res.header("Access-Control-Allow-Origin", "*");
   return 0
